aaea67936c1d80e113a5bb1b4d6e9aa0fc87cf6a
Wenszel <wiktor.smaga13@gmail.com>
2023-03-29 08:33:49 +0200

    improved col/2022/1a algorithm

--- colloquiums/2022/1a/kol1a.py
+++ colloquiums/2022/1a/kol1a.py
@@ -1,11 +1,5 @@
 from kol1atesty import runtests
 
-def string_score(string):
-    suma = 0
-    for letter in string:
-        suma += ord(letter)
-    return suma
-
 
 def is_equivalent(s1, s2):
     return s1 == s2 or s1 == s2[::-1]
@@ -13,7 +7,7 @@ def is_equivalent(s1, s2):
 
 def qs_len(array, p, l):
     top = 0
-    stos = [0 for _ in range(len(array))]
+    stos = [(0, 0) for _ in range(len(array))]
     stos[top] = p, l
     top += 1
     while top > 0:
@@ -32,7 +26,7 @@ def partition(array, start, end):
     pivot = array[end]
     i = start
     for j in range(start, end):
-        if len(array[j]) <= len(pivot) and not is_equivalent(array[j], pivot):
+        if array[j] <= pivot and not is_equivalent(array[j], pivot):
             array[i], array[j] = array[j], array[i]
             i += 1
     array[i], array[end] = array[end], array[i]
@@ -40,12 +34,14 @@ def partition(array, start, end):
 
 
 def g(T):
-    # tu prosze wpisac wlasna implementacje
-    qs_len(T, 0, len(T)-1)
+    for i in range(len(T) - 1):
+        if T[i][0] > T[i][-1]:
+            T[i] = T[i][::-1]
+    qs_len(T, 0, len(T) - 1)
     biggest = 0
     streak = 1
-    for i in range(len(T)-1):
-        if is_equivalent(T[i], T[i+1]):
+    for i in range(len(T) - 1):
+        if is_equivalent(T[i], T[i + 1]):
             streak += 1
         else:
             if biggest < streak:
@@ -54,5 +50,4 @@ def g(T):
     return biggest
 
 
-# Zamien all_tests=False na all_tests=True zeby uruchomic wszystkie testy
-runtests( g, all_tests=True )
+runtests(g, all_tests=True)

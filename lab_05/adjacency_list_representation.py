473817b34f990fe6a0b8459a42bd3b118fc936b1
Wenszel <wiktor.smaga13@gmail.com>
2023-04-19 18:09:00 +0200

    added the adjacency list representation of a graph

---
+++ lab_05/adjacency_list_representation.py
@@ -0,0 +1,17 @@
+class Graph:
+    def __init__(self, v, e):
+        self.v = v
+        self.e = e
+        self.l = self.create_adjacency_list()
+
+    def create_adjacency_list(self):
+        n = len(self.v)
+        adj_list = [[] for _ in range(n)]
+        for edge in self.e:
+            adj_list[edge[0]].append(edge[1])
+            adj_list[edge[1]].append(edge[0])
+        return adj_list
+
+    def print_list(self):
+        for i in range(len(self.l)):
+            print(i, "->", self.l[i])

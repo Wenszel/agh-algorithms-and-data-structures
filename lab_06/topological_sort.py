9b086e06b741fc418cdc48397c4fb6288b305cc9
Wenszel <wiktor.smaga13@gmail.com>
2023-04-22 16:20:34 +0200

    added topological sort

---
+++ lab_06/topological_sort.py
@@ -0,0 +1,25 @@
+from lab_05 import adjacency_list_representation
+
+def topological_sort(G, start):
+    n = len(G)
+    visited = [False] * n
+    sorted_vertices = []
+    for vertex in G[start]:
+        if not visited[vertex]:
+            dfs_visited(G, visited, vertex, sorted_vertices)
+    sorted_vertices.insert(0, start)
+    return sorted_vertices
+def dfs_visited(G, visited, vertex, sorted_vertices):
+    visited[vertex] = True
+    for neighbour in G[vertex]:
+        if not visited[neighbour]:
+            dfs_visited(G, visited, neighbour, sorted_vertices)
+    sorted_vertices.insert(0, vertex)
+
+
+vertices = [i for i in range(8)]
+edges = [(0, 1), (0, 2), (1, 3), (3, 5), (5, 6), (5, 7), (4, 5), (2, 4)]
+graph = adjacency_list_representation.Graph(vertices, edges, True)
+graph.print_list()
+print(topological_sort(graph.l, 0))
+

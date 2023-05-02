e0cf65aa5312c2832130b273b0c201977df841af
Wenszel <wiktor@smaga.dev>
2023-05-02 15:58:51 +0200

    improved solution to 5th offline task

--- offline/2023/t_05/zad5.py
+++ offline/2023/t_05/zad5.py
@@ -1,25 +1,25 @@
 from zad5testy import runtests
 from queue import PriorityQueue
 
-def path_finding(vertices, a, b):
+
+def path_finding(vertices, a, b, S):
     distances = [float('inf') for _ in range(len(vertices))]
     queue = PriorityQueue()
     queue.put((0, a))
-
+    flag = False
     while not queue.empty():
         distance, vertex = queue.get()
         if distances[vertex] == float('inf'):
             distances[vertex] = distance
             for v in vertices[vertex]:
                 queue.put((distance + v[1], v[0]))
-    return distances[b]
+            if not flag and vertex in S:
+                for s in S:
+                    queue.put((distance, s))
+                flag = True
+        if vertex == b:
+            return distances[b]
 
-
-def add_singularity_edges(E, S):
-    n = len(S)
-    for i in range(n):
-        for j in range(i, n):
-            E.append((S[i], S[j], 0))
 
 def create_adjacency_list(E, n):
     adjacency_list = [[] for _ in range(n)]
@@ -28,12 +28,11 @@ def create_adjacency_list(E, n):
         adjacency_list[edge[1]].append((edge[0], edge[2]))
     return adjacency_list
 
+
 def spacetravel(n, E, S, a, b):
-    add_singularity_edges(E, S)
     adjacency_list = create_adjacency_list(E, n)
-    time = path_finding(adjacency_list, a, b)
+    time = path_finding(adjacency_list, a, b, S)
     return None if time == float('inf') else time
 
 
-
-runtests(spacetravel, all_tests=False)
+runtests(spacetravel, all_tests=True)

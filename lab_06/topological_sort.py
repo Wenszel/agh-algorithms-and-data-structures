a573a51d4a00ca3e4e93df9ae481ae3fb8159fdd
Wenszel <wiktor@smaga.dev>
2023-05-03 21:26:49 +0200

    refactored to export topological sort

--- lab_06/topological_sort.py
+++ lab_06/topological_sort.py
@@ -1,14 +1,15 @@
 from lab_05 import adjacency_list_representation
 
-def topological_sort(G, start):
+def topological_sort(G):
     n = len(G)
     visited = [False] * n
     sorted_vertices = []
-    for vertex in G[start]:
+    for vertex in range(n):
         if not visited[vertex]:
             dfs_visited(G, visited, vertex, sorted_vertices)
-    sorted_vertices.insert(0, start)
     return sorted_vertices
+
+
 def dfs_visited(G, visited, vertex, sorted_vertices):
     visited[vertex] = True
     for neighbour in G[vertex]:
@@ -17,9 +18,12 @@ def dfs_visited(G, visited, vertex, sorted_vertices):
     sorted_vertices.insert(0, vertex)
 
 
-vertices = [i for i in range(8)]
-edges = [(0, 1), (0, 2), (1, 3), (3, 5), (5, 6), (5, 7), (4, 5), (2, 4)]
-graph = adjacency_list_representation.Graph(vertices, edges, True)
-graph.print_list()
-print(topological_sort(graph.l, 0))
+def run_topological_sort():
+    vertices = [i for i in range(8)]
+    edges = [(0, 1), (0, 2), (1, 3), (3, 5), (5, 6), (5, 7), (4, 5), (2, 4)]
+    graph = adjacency_list_representation.Graph(vertices, edges, True)
+    graph.print_list()
+    print(topological_sort(graph.l))
+
+# run_topological_sort()
 

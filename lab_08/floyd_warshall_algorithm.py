1dfd8a8f31ee9253bc5365555b3ed9e5b5b27cc2
Wenszel <wiktor.smaga13@gmail.com>
2023-05-19 11:50:29 +0200

    renamed variables in Floyd-Warshall algorithm

--- lab_08/floyd_warshall_algorithm.py
+++ lab_08/floyd_warshall_algorithm.py
@@ -1,20 +1,33 @@
 import numpy as np
+
+
 def floyd_warshall(G):
     n = len(G)
     distance = G
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+    for intermediate in range(n):
+        for start in range(n):
+            if intermediate == start:
+                continue
+            for end in range(n):
+                if intermediate == end or start == end:
+                    continue
+                distance[start][end] = min(distance[start][end],
+                                           distance[start][intermediate] + distance[intermediate][end])
     return distance
 
 
 INF = float('inf')
-graph = [
-    [0,   5, INF, 10],
-    [INF,  0,  3,  INF],
-    [INF, INF, 0,   1],
+graph1 = [
+    [0, 5, INF, 10],
+    [INF, 0, 3, INF],
+    [INF, INF, 0, 1],
     [INF, INF, INF, 0]
 ]
-
-print(np.matrix(floyd_warshall(graph)))
+graph2 = [
+    [0, 3, INF, 7],
+    [8, 0, 2, INF],
+    [5, INF, 0, 1],
+    [2, INF, INF, 0]
+]
+print(np.matrix(floyd_warshall(graph1)))
+print(np.matrix(floyd_warshall(graph2)))

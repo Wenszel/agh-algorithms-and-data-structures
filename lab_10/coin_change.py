b735c4cef423c98eee314dd4a58c3b4bab088d86
Wenszel <wiktor.smaga13@gmail.com>
2023-06-02 16:35:41 +0200

    added solution to coin change problem

---
+++ lab_10/coin_change.py
@@ -0,0 +1,11 @@
+def coin_change(system, value):
+    counts = [float('inf')] * (value + 1)
+    counts[0] = 0
+    for i in range(1, value + 1):
+        for coin in system:
+            if coin <= i and counts[i - coin] != -1:
+                counts[i] = min(counts[i - coin] + 1, counts[i])
+    return counts[value]
+
+
+print(coin_change([186, 419, 83, 408], 6249))
